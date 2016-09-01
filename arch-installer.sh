60f596ed0f940ed8ab94cac2c549627c034a52a3
condor2 <condor20_05@yahoo.it>
2016-09-01 10:37:43 +0300

    fixed minimal plasma installation

--- arch-installer.sh
+++ arch-installer.sh
@@ -1677,7 +1677,7 @@ graphics() {
 					 start_term="exec mate-session"
 		;;
 		"KDE plasma")	if (dialog --defaultno --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg3" 10 60) then
-							DE="plasma-desktop"
+							DE="plasma-desktop sddm"
 						else
 							DE="kde-applications plasma"
 						fi
