2baf8af701edacf0a47911d5d0e4418386f933f8
deadhead420 <deadhead3492@gmail.com>
2016-09-09 16:17:32 -0400

    update

--- arch-installer.sh
+++ arch-installer.sh
@@ -22,7 +22,7 @@
 ### but WITHOUT ANY WARRANTY; without even the implied warranty of
 ### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ### GNU General Public License version 2 for more details.
-###############################################################
+################################################################
 
 init() {
 
