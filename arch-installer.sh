4219d696cbc94bb30a1f80094ef63b92a487417b
condor2 <condor20_05@yahoo.it>
2016-11-29 22:04:56 +0200

    Update arch-installer.sh

--- arch-installer.sh
+++ arch-installer.sh
@@ -1186,52 +1186,61 @@ graphics() {
 								start_term="exec startxfce4" de_config=true
 		;;
 		"xfce4") 	if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg0" 10 60) then
-						DE="xfce4 xfce4-goodies"
+						DE="xfce4 xfce4-goodies xdg-user-dirs"
+					else
+						DE="xdg-user-dirs"
 					fi
 					start_term="exec startxfce4"
 		;;
 		"budgie")	if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg6" 10 60) then
-						DE="budgie-desktop gnome"
+						DE="budgie-desktop gnome xdg-user-dirs"
 					else
-						DE="budgie-desktop"
+						DE="budgie-desktop xdg-user-dirs"
 					fi
 					start_term="export XDG_CURRENT_DESKTOP=Budgie:GNOME ; exec budgie-desktop"
 		;;
 		"gnome")	if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg1" 10 60) then
-						DE="gnome gnome-extra"
+						DE="gnome gnome-extra xdg-user-dirs"
+					else
+						DE="xdg-user-dirs"
 					fi
 					 start_term="exec gnome-session"
 		;;
 		"mate")		if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg2" 10 60) then
-						DE="mate mate-extra"
+						DE="mate mate-extra xdg-user-dirs"
+					else
+						DE="xdg-user-dirs"
 					fi
 					 start_term="exec mate-session"
 		;;
 		"KDE plasma")	if (dialog --defaultno --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg3" 10 60) then
-						DE="plasma-desktop sddm konsole dolphin plasma-nm plasma-pa breeze-kde4"
-					else
-						DE="plasma kde-applications"
-					fi
+							DE="plasma-desktop sddm konsole dolphin plasma-nm plasma-pa breeze-kde4 xdg-user-dirs"
+						else
+							DE="plasma kde-applications xdg-user-dirs"
+						fi
 						
 						enable_dm=true
 						start_term="exec startkde"
 		;;
 		"deepin")	if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg4" 10 60) then
-						DE="deepin deepin-extra"
+						DE="deepin deepin-extra xdg-user-dirs"
+					else
+						DE="xdg-user-dirs"
 					fi
  					start_term="exec startdde"
  		;;
  		"xmonad")	if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg5" 10 60) then 
                         			DE="xmonad xmonad-contrib"
+					else
+						DE="xdg-user-dirs"
                     			fi
                     			start_term="exec xmonad"
 		;;	
-		"cinnamon") start_term="exec cinnamon-session"
-		
+		"cinnamon") start_term="exec cinnamon-session" 
 		;;
 		"lxde") 	if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$gtk3_msg0" 10 60) then 
-                        			DE="lxde-gtk3"
-                    			fi				
+                        DE="lxde-gtk3"
+                    fi
 					start_term="exec startlxde" 
 		;;
 		"lxqt") 	start_term="exec startlxqt" 
@@ -1244,19 +1253,14 @@ graphics() {
 					DE="bspwm sxhkd"
 		;;
 		"fluxbox")	start_term="exec startfluxbox" 
-		
 		;;
 		"openbox")	start_term="exec openbox-session"
-		
 		;;
 		"awesome") 	start_term="exec awesome" 
-		
 		;;	
 		"dwm") 		start_term="exec dwm" 
-		
 		;;
 		"i3") 		start_term="exec i3" 
-		
 		;;
 	esac
 
