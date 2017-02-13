dba4ee30a54f4b5ebaea5b632ceedb8b10c6fef8
Plamen Nedkov <pnedkov@gmail.com>
2017-02-13 14:50:18 -0800

    Properly handle the decimal comma

--- arch-installer.sh
+++ arch-installer.sh
@@ -674,7 +674,7 @@ part_class() {
 
 		if [ -z "$ROOT" ]; then
 			case "$part_size" in
-				[4-9]G|[0-9][0-9]*G|[4-9].*G|T)
+				[4-9]G|[0-9][0-9]*G|[4-9].*G|[4-9],*G|T)
 					if (dialog --yes-button "$yes" --no-button "$no" --defaultno --yesno "\n$root_var" 13 60) then
 						f2fs=$(cat /sys/block/$(echo $part | sed 's/[0-9]//g')/queue/rotational)
 						fs_select
