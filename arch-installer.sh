09a77e0be376ad83722f550682e50788ffc89d64
deadhead420 <deadhead3492@gmail.com>
2017-03-07 18:15:33 -0500

    Fixed write-changes button manual partitioning

--- arch-installer.sh
+++ arch-installer.sh
@@ -667,6 +667,7 @@ part_menu() {
 	echo "\"$done_msg\" \"$write\" 3>&1 1>&2 2>&3" >> "$tmp_menu"
 	echo "if [ \"\$?\" -eq \"3\" ]; then clear ; echo \"$done_msg\" ; fi" >> "$tmp_menu"
 	part=$(bash "$tmp_menu" | sed 's/^\s\+//g;s/\s\+$//g')
+	if (<<<"$part" grep "$done_msg" &> /dev/null) then part="$done_msg" ; fi
 	rm $tmp_menu $tmp_list
 	part_class
 
