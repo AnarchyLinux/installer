9652a4fd28f3590b49fc1cac9b9f885be9f6bc69
deadhead420 <deadhead3492@gmail.com>
2017-03-16 15:58:06 -0400

    Added license

--- arch-installer.sh
+++ arch-installer.sh
@@ -2,7 +2,7 @@
 ###############################################################
 ### Arch Linux Anywhere Install Script
 ###
-### Copyright (C) 2017  Dylan Schacht
+### Copyright (C) 2017 Dylan Schacht
 ###
 ### By: Dylan Schacht (deadhead)
 ### Email: deadhead3492@gmail.com
@@ -21,7 +21,11 @@
 ### This program is distributed in the hope that it will be useful,
 ### but WITHOUT ANY WARRANTY; without even the implied warranty of
 ### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-### GNU General Public License version 2 for more details.
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program; if not, write to the Free Software
+### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ################################################################
 
 init() {
