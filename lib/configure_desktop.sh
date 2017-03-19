34642a6a1a04621fb86590f23f044c504afeafa5
condor2 <condor20_05@yahoo.it>
2017-03-19 14:30:33 +0200

    removed mate-gtk2

--- lib/configure_desktop.sh
+++ lib/configure_desktop.sh
@@ -94,6 +94,7 @@ graphics() {
 					else
 						DE="mate gtk-engine-murrine"
 					fi
+				GTK3=false
 				start_term="exec mate-session"
 		;;
 		"KDE plasma")	if (dialog --defaultno --yes-button "$yes" --no-button "$no" --yesno "\n$extra_msg3" 10 60) then
