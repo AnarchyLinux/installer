51b9462bf8e2732c148d4e02cfe38932f57a0782
deadhead420 <deadhead3492@gmail.com>
2017-04-05 23:52:59 -0400

    Bug fixes

--- lib/install_base.sh
+++ lib/install_base.sh
@@ -35,63 +35,41 @@ install_base() {
 		height="16"
 	fi
 
-	while ! "$INSTALLED"
-	  do
-		if (dialog --yes-button "$install" --no-button "$cancel" --yesno "\n$install_var" "$height" 65); then
-			echo "$(date -u "+%F %H:%M") : Begin base install" >> "$log"
-    
-			if [ "$kernel" == "linux" ]; then
-				base_install="$(pacman -Sqg base) $base_install"
-			else
-				base_install="$(pacman -Sqg base | sed 's/^linux//') $base_install"
-			fi
-    
-			(pacstrap "$ARCH" --force $(echo "$base_install") ; echo "$?" > /tmp/ex_status) &>> "$log" &
-			pid=$! pri=$(echo "$down" | sed 's/\..*$//') msg="\n$install_load_var" load_log
-    
-			genfstab -U -p "$ARCH" >> "$ARCH"/etc/fstab
-    
-			if [ $(</tmp/ex_status) -eq "0" ]; then
-				INSTALLED=true
-				echo "$(date -u "+%F %H:%M") : Install Complete" >> "$log"
-				echo "$(date -u "+%F %H:%M") : Generate fstab:\n$(<$ARCH/etc/fstab)" >> "$log"
-			else
-				dialog --ok-button "$ok" --msgbox "\n$failed_msg" 10 60
-				echo "$(date -u "+%F %H:%M") : Install failed: please report to developer" >> "$log"
-				reset ; tail "$log" ; exit 1
-			fi
- 
-### Intel microcode is broken as fuck its done nothing but piss me off
-### If you want to try fixing this be my guest but its a buggy piece of crap and I'm done with it
-###
-#			if "$intel" && ! "$VM" ; then
-#				if (dialog --yes-button "$install" --no-button "$cancel" --yesno "\n$ucode_msg" 11 60); then
-#					arch-chroot "$ARCH" pacman -Sy intel-ucode &>/dev/null &
-#					pid=$! pri=1 msg="\n$wait_load \n\n \Z1> \Z2pacman -Sy intel-ucode\Zn" load
-#   
-#					if [ -f "$ARCH/boot/intel-ucode.img" ]; then
-#						ucode=true
-#						echo "$(date -u "+%F %H:%M") : Installed intel-ucode" >> "$log"
-#					else
-#						dialog --ok-button "$ok" --msgbox "\n$ucode_failed_msg" 10 60
-#						echo "$(date -u "+%F %H:%M") : intel-ucode install failed" >> "$log"
-#					fi
-#				fi
-#			fi
-    
-			case "$bootloader" in
-				grub) grub_config ;;
-				syslinux) syslinux_config ;;
-				systemd-boot) systemd_config ;;
-			esac
-    
-			echo "$(date -u "+%F %H:%M") : Configured bootloader: $bootloader" >> "$log"
-			configure_system
+	if (dialog --yes-button "$install" --no-button "$cancel" --yesno "\n$install_var" "$height" 65); then
+		echo "$(date -u "+%F %H:%M") : Begin base install" >> "$log"
+
+		if [ "$kernel" == "linux" ]; then
+			base_install="$(pacman -Sqg base) $base_install"
 		else
-			if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$exit_msg" 10 60) then
-				main_menu
-			fi
+			base_install="$(pacman -Sqg base | sed 's/^linux//') $base_install"
 		fi
-	done
+
+		(pacstrap "$ARCH" --force $(echo "$base_install") ; echo "$?" > /tmp/ex_status) &>> "$log" &
+		pid=$! pri=$(echo "$down" | sed 's/\..*$//') msg="\n$install_load_var" load_log
+
+		genfstab -U -p "$ARCH" >> "$ARCH"/etc/fstab
+
+		if [ $(</tmp/ex_status) -eq "0" ]; then
+			INSTALLED=true
+			echo "$(date -u "+%F %H:%M") : Install Complete" >> "$log"
+			echo "$(date -u "+%F %H:%M") : Generate fstab:\n$(<$ARCH/etc/fstab)" >> "$log"
+		else
+			dialog --ok-button "$ok" --msgbox "\n$failed_msg" 10 60
+			echo "$(date -u "+%F %H:%M") : Install failed: please report to developer" >> "$log"
+			reset ; tail "$log" ; exit 1
+		fi
+
+		case "$bootloader" in
+			grub) grub_config ;;
+			syslinux) syslinux_config ;;
+			systemd-boot) systemd_config ;;
+		esac
+
+		echo "$(date -u "+%F %H:%M") : Configured bootloader: $bootloader" >> "$log"
+	else
+		if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$exit_msg" 10 60) then
+			main_menu
+		fi
+	fi
 
 }
