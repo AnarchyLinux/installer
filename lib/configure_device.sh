daf00a20911f76632a183e8b3105fc4fcf3df1b1
Plamen Nedkov <pnedkov@gmail.com>
2017-04-11 11:26:54 -0700

    Better way of determining if device is rotational

--- lib/configure_device.sh
+++ lib/configure_device.sh
@@ -62,7 +62,7 @@ prepare_drives() {
 				drive_byte=$(lsblk -nibo NAME,SIZE | grep -w "$DRIVE" | awk '{print $2}')
 				drive_mib=$((drive_byte/1024/1024))
 				drive_gigs=$((drive_mib/1024))
-				f2fs=$(cat /sys/block/"$DRIVE"/queue/rotational)
+				f2fs=$(lsblk -dnro ROTA /dev/$DRIVE)
 				echo "$(date -u "+%F %H:%M") : Drive size in MB: $drive_mib" >> "$log"
 				echo "$(date -u "+%F %H:%M") : F2FS state: $f2fs" >> "$log"
 				fs_select
@@ -614,11 +614,7 @@ part_class() {
 			case "$part_size" in
 				[4-9]G|[1-9][0-9]*G|[4-9].*G|[4-9],*G|T)
 					if (dialog --yes-button "$yes" --no-button "$no" --defaultno --yesno "\n$root_var" 13 60) then
-						if (<<<"$part" egrep "md[0-9]+$" &> /dev/null); then
-							f2fs=$(cat /sys/block/$part/queue/rotational)
-						else
-							f2fs=$(cat /sys/block/$(echo $part | sed 's/[0-9]\+$//;/[0-9]/s/p$//')/queue/rotational)
-						fi
+						f2fs=$(lsblk -dnro ROTA /dev/$part)
 						fs_select
 
 						if [ "$?" -gt "0" ]; then
@@ -730,7 +726,7 @@ part_class() {
 
 			if [ "$mnt" != "SWAP" ]; then
 				if (dialog --yes-button "$yes" --no-button "$no" --defaultno --yesno "\n$part_frmt_msg" 11 50) then
-					f2fs=$(cat /sys/block/$(echo $part | sed 's/[0-9]\+$//;/[0-9]/s/p$//')/queue/rotational)
+					f2fs=$(lsblk -dnro ROTA /dev/$part)
 
 					if [ "$mnt" == "/boot" ] || [ "$mnt" == "/boot/EFI" ] || [ "$mnt" == "/boot/efi" ]; then
 						f2fs=1
