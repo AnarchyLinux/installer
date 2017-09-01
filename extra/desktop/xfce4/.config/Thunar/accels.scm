00d3f407d799550187de601fc0ec1865ac6787e6
deadhead420 <deadhead3492@gmail.com>
2017-09-01 17:44:41 -0400

    update xfce desktop

--- extra/desktop/xfce4/.config/Thunar/accels.scm
+++ extra/desktop/xfce4/.config/Thunar/accels.scm
@@ -29,6 +29,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-side-pane-tree" "<Primary>e")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/restore" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/open-network" "")
+; (gtk_accel_path "<Actions>/ThunarActions/Tap::create-archive" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/new-tab" "<Primary>t")
 ; (gtk_accel_path "<Actions>/ThunarActions/uca-action-1455850311268596-1" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/preferences" "")
