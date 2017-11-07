54a6869414997602bc82357a0c44d7f2736e7bd8
deadhead420 <deadhead3492@gmail.com>
2017-11-07 06:34:58 -0500

    unset variables if user cancels

--- lib/install_base.sh
+++ lib/install_base.sh
@@ -20,7 +20,7 @@ install_base() {
 	op_title="$install_op_msg"
 	base_install=$(<<<"$base_install" tr " " "\n" | sort | uniq | tr "\n" " ")
 	pacman -Sy --print-format='%s' $(echo "$base_install") | awk '{s+=$1} END {print s/1024/1024}' >/tmp/size &
-	pid=$! pri=0.6 msg="\n$pacman_load \n\n \Z1> \Z2pacman -Sy\Zn" load	
+	pid=$! pri=0.6 msg="\n$pacman_load \n\n \Z1> \Z2pacman -Sy\Zn" load
 	download_size=$(</tmp/size) ; rm /tmp/size
 	export software_size=$(<<<"$download_size" sed 's/\(\..\)\(.*\)/\1 MiB/')
 	cal_rate
@@ -39,18 +39,18 @@ install_base() {
 	  do
 		if (dialog --yes-button "$install" --no-button "$cancel" --yesno "\n$install_var" "$height" 65); then
 			echo "$(date -u "+%F %H:%M") : Begin base install" >> "$log"
-    
+
 			if [ "$kernel" == "linux" ]; then
 				base_install="$(pacman -Sqg base) $base_install"
 			else
 				base_install="$(pacman -Sqg base | sed 's/^linux//') $base_install"
 			fi
-    
+
 			(pacstrap "$ARCH" --force $(echo "$base_install") ; echo "$?" > /tmp/ex_status) &>> "$log" &
 			pid=$! pri=$(echo "$down" | sed 's/\..*$//') msg="\n$install_load_var" load_log
-    
+
 			genfstab -U -p "$ARCH" >> "$ARCH"/etc/fstab
-    
+
 			if [ $(</tmp/ex_status) -eq "0" ]; then
 				INSTALLED=true
 				echo "$(date -u "+%F %H:%M") : Install Complete" >> "$log"
@@ -60,17 +60,19 @@ install_base() {
 				echo "$(date -u "+%F %H:%M") : Install failed: please report to developer" >> "$log"
 				reset ; tail "$log" ; exit 1
 			fi
-    
+
 			case "$bootloader" in
 				grub) grub_config ;;
 				syslinux) syslinux_config ;;
 				systemd-boot) systemd_config ;;
 				efistub) efistub_config ;;
 			esac
-    
+
 			echo "$(date -u "+%F %H:%M") : Configured bootloader: $bootloader" >> "$log"
 		else
 			if (dialog --yes-button "$yes" --no-button "$no" --yesno "\n$exit_msg" 10 60) then
+				unset base_install DE
+				desktop=false
 				main_menu
 			fi
 		fi
