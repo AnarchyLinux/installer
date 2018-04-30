ea4caa4be48aca5d530802c26fee4ad7f3e9c4ae
condor2 <condor20_05@yahoo.it>
2018-04-30 08:56:21 +0300

    Replaced qt4 vlc with q5

--- lib/configure_base.sh
+++ lib/configure_base.sh
@@ -489,7 +489,7 @@ add_software() {
 						add_soft=false
 					fi
 					if (<<<"$software" grep "vlc") then
-						software+=" qt4 phonon-qt4-vlc"
+						software+=" qt5 phonon-qt5-vlc"
 					fi
 					if (<<<"$software" grep "multimedia-codecs") then
 						software=$(<<<"$software" sed 's/multimedia-codecs/gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly ffmpegthumbnailer gst-libav/')
