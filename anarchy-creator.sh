00a8494f4240fe48dedf6b253ec5e40ec77f2760
condor2 <condor20_05@yahoo.it>
2018-09-30 14:50:22 +0300

    Update anarchy-creator.sh

--- anarchy-creator.sh
+++ anarchy-creator.sh
@@ -21,7 +21,7 @@ set_version() {
 	export iso_rel="1.0.2"
 
 	### Note ISO label must remain 11 characters long:
-	export iso_label="ANARCHYV101"
+	export iso_label="ANARCHYV102"
 
 	### ISO name
 	case "$interface" in
