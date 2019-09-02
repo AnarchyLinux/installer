0a126c7181afa35e323294f718d12f56952b9e5e
Erazem Kokot <contact@erazem.eu>
2019-09-02 11:56:01 +0200

    Create deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if ["`git status-s`"]; then
+	echo "The working directory is dirty. Please commit any pending changes."
+	exit 1;
+fi
+
+echo "Deleting old publication (public folder)"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publish to gh-pages (deploy.sh)"
+
+echo "For security reasons you have to manually push to upstream"
+echo "e.g. 'git push upstream gh-pages'"
