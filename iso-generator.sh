6ff3f7631229ee72f6b9b7e96c03cc5020cc58f0
Erazem Kokot <contact@erazem.eu>
2019-09-27 00:07:50 +0200

    Create sha256sum inside the out directory
    sha256sum didn't find the generated iso since it was moved,
    now the checksum is generated inside the output directory.

--- iso-generator.sh
+++ iso-generator.sh
@@ -409,8 +409,8 @@ create_iso() {
 generate_checksums() {
     echo -e "Generating image checksum ..." | log
     local sha_256_sum
-    sha_256_sum=$(sha256sum "${anarchy_iso_name}")
-    echo -e "${sha_256_sum}" > "${anarchy_iso_name}".sha256sum
+    sha_256_sum=$(sha256sum "${out_dir}"/"${anarchy_iso_name}")
+    echo -e "${sha_256_sum}" > "${out_dir}"/"${anarchy_iso_name}".sha256sum
     echo -e "Done generating image checksum"
     echo -e ""
 }
