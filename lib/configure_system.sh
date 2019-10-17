e263515763ca3d797a3c3dc39c27cda527d45737
Erazem Kokot <contact@erazem.eu>
2019-10-18 00:08:47 +0200

    Remove i686 support, add AMD drivers and update wallpaper download procedure (#900)
    * Remove i686 code from scripts
    
    * Update wallpaper download procedure (remove wallpapers folder and clone from brand directory when generating iso)
    
    * Add AMDGPU driver (`xf86-video-amdgpu`) to menu and add a translatable variable for it (gr10)
    
    * Add output and log directory arguments to help menu (`-o`/`--output-dir` and `-l`/`--log-dir`)

--- lib/configure_system.sh
+++ lib/configure_system.sh
@@ -205,7 +205,7 @@ configure_system() {
     fi
 
     if "$aa_repo" ; then
-        sed -i -e '$a\\n[anarchy]\nServer = https://anarchylinux.org/repo/$arch\nSigLevel = Never' "$ARCH"/etc/pacman.conf
+        sed -i -e '$a\\n[anarchy]\nServer = https://anarchylinux.org/repo/x86_64\nSigLevel = Never' "$ARCH"/etc/pacman.conf
     fi
 
     if "$dhcp" ; then
